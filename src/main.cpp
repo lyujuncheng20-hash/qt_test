3dd6c5db4dba4d94b00f77939989b63c7edbdd3d
lyujuncheng <lyujuncheng20@gmail.com>
2026-06-03 14:05:15 +0900

    1

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,12 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
-    // 通知 QML 引擎，去当前 exe 所在的同级目录（即 bin 目录）下寻找 QML 模块
-    engine.addImportPath(QCoreApplication::applicationDirPath());
-
-    // 强行激活 c++ 模块的符号绑定，防止编译器优化掉动态库链接
-    qDebug() << "Linking QML Module C++ Backend..." << Counter::staticMetaObject.className();
-
     Logger logger;
     engine.rootContext()->setContextProperty("cppLogger", &logger);
 
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
                      &app, []() { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
 
-    // 标准加载方式
     engine.loadFromModule("app_ui", "Main");
 
     return app.exec();
